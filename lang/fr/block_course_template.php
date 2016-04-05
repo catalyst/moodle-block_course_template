441ed8ec9389954c55dc709e8b4892993f48ee96
Stacey Walker <stacey@catalyst-eu.net>
2016-04-05 16:26:21 +0100

    WR253634: added French language translations
    Change-Id: I5556fd9f44102c698be6a2dff4a14f1d3ed55542

---
+++ lang/fr/block_course_template.php
@@ -0,0 +1,119 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://academydev.catalystdemo.co.uk
+ *
+ * @package    block
+ * @subpackage course_template
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['actions'] = 'Actions';
+$string['activetags'] = 'Tags actifs';
+$string['addtemplate'] = 'Ajouter un modèle de cours';
+$string['alltemplates'] = 'Voir tous les modèles de cours';
+$string['basedon'] = 'basé sur « ${a} »';
+$string['basedoncourse'] = 'Basé sur le cours : « ${a} »';
+$string['basedontemplate'] = 'Basé sur le modèle : « ${a} »';
+$string['channeltemplates'] = 'Modèles de flux';
+$string['configdescription'] = 'Définissez quels formats de cours peuvent être utilisés pour créer des modèles de cours';
+$string['confirmdelete'] = 'Êtes-vous sûr de vouloir supprimer le modèle « {$a} » ?';
+$string['courseformats'] = 'Formats de cours';
+$string['course_template:addinstance'] = 'Ajouter un bloc de modèle de cours';
+$string['course_template:createcourse'] = 'Créer un nouveau cours depuis un modèle';
+$string['course_template:duplicatecourse'] = 'Dupliquer le cours';
+$string['course_template:edit'] = 'Créer et administrer des modèles de cours';
+$string['course_template:import'] = 'Importer un modèle dans un cours';
+$string['course_template:managechannels'] = 'Gérer les flux de formation';
+$string['course_template:myaddinstance'] = 'Ajouter un bloc de modèle de cours';
+$string['coursetemplates'] = 'Modèles de cours';
+$string['course_template:view'] = 'Voir les modèles de cours disponibles';
+$string['course_template:viewchannel'] = 'Voir un flux de formation';
+$string['createcourse'] = 'Nouveau cours à partir d\'un modèle';
+$string['createdsuccessfully'] = 'Votre cours a été crée avec succès';
+$string['createtemplate'] = 'Créer un modèle';
+$string['customcourseheading'] = 'Entête de flux de formation';
+$string['customcourseheading_help'] = 'Les flux de formation peuvent avoir une entête personnalisée. Si rien n\'est ajouté ici, le nom complet du cours sera utilisé.';
+$string['deletetemplate'] = 'Supprimer le modèle';
+$string['details'] = 'Détails';
+$string['duplicatecourse'] = 'Dupliquer le cours';
+$string['duplicatecoursesuccess'] = 'Le course a été dupliqué.</br>
+Veuillez éditer le cours ci-dessous.';
+$string['edittempdata'] = 'Éditer les données du modèle';
+$string['edittemplate'] = 'Éditer le modèle de cours';
+$string['edittmptitle'] = 'Éditer le modèle de cours';
+$string['enrolment'] = 'Inscription';
+$string['enrolment_help'] = 'Voulez-vous copier les données d\'inscription des utilisateurs ?';
+$string['error:couldnotinserttag'] = 'Impossible d\'insérer le tag {$a}';
+$string['error:couldntgetcourse'] = 'Impossible de trouver un cours avec l\'identifiant « {$a} »';
+$string['error:couldntgettag'] = 'Impossible de trouver un tag avec l\'identifiant « {$a} »';
+$string['error:couldntupdate'] = 'Impossible de mettre à jour le modèle de cours {$a}';
+$string['error:createtemplatefile'] = 'Impossible de créer un fichier de sauvegarde pour le modèle {$a}';
+$string['error:deleteinst'] = 'Impossible de supprimer les instances de tag';
+$string['error:deletetag'] = 'Impossible de supprimer les tags';
+$string['error:extractarchive'] = 'Impossible d\'extraire l\'archive';
+$string['error:movearchive'] = 'Impossible de copier l\'archive';
+$string['error:nameempty'] = 'Le champ de nom ne doit pas être vide.';
+$string['error:nametaken'] = 'Ce nom de modèle est déjà utilisé. Veuillez en choisir un autre.';
+$string['error:needsconfig'] = 'L\'emplacement des sauvegardes automatiques n\'a pas été configuré, et cela est requis pour que les modèles de cours fonctionnent. Veuillez demander à un administrateur de le configurer.';
+$string['error:nodirectory'] = 'Aucun répertoire de sauvegarde des modèles';
+$string['error:notemplate'] = 'Aucun enregistrement pour le modèle d\'identifiant {$a}';
+$string['error:notemplates'] = 'Aucun modèle de cours disponible pour créer ce cours';
+$string['error:paramrequired'] = 'Paramètre manquant - vous devez spécifier soit le cours soit le modèle';
+$string['error:processerror'] = 'Impossible de traiter le fichier d\'archive.';
+$string['error:save'] = 'Impossible de sauvegarder le modèle.';
+$string['error:sitecourse'] = 'Le cours de contexte du site ne peut PAS être utilisé.';
+$string['existingtags'] = 'Tags existants';
+$string['filtertemplates'] = 'Rafraîchir les modèles';
+$string['filtertext'] = 'N\'afficher que les modèles taggés avec :';
+$string['goto'] = 'Aller à';
+$string['importedsuccessfully'] = 'Votre cours a été importé avec succès';
+$string['importintocourse'] = 'Importer un modèle dans un cours';
+$string['intocourse'] = 'Importer un modèle dans ce cours';
+$string['lastmodifiedon'] = 'Dernière modification : {$a}';
+$string['name'] = 'Nom du cours';
+$string['name_help'] = 'Veuillez renseigner le nom du nouveau cours';
+$string['newcoursefromtemp'] = 'Nouveau cours à partir d\'un modèle';
+$string['newcoursetemplate'] = 'Nouveau modèle de cours';
+$string['newtemplate'] = 'Modèle à partir de ce cours';
+$string['noimage'] = 'Pas d\'image';
+$string['norecords'] = 'Il n\'y a aucun modèle à afficher';
+$string['notags'] = 'Il n\'y a actuellement aucun tag défini.';
+$string['pagesize'] = 'Taille de la page';
+$string['pagesize_desc'] = 'Définissez combien de modèles doivent être affichés par page.';
+$string['pluginname'] = 'Modèles de cours';
+$string['preview'] = 'Prévisualisation';
+$string['savesuccess'] = 'Les paramètres de votre modèle ont été sauvegardés avec succès';
+$string['screenshot'] = 'Capture d\'écran';
+$string['screenshot_help'] = 'Les fichiers de capture d\'écran, tels que les images, sont affichés dans la liste des modèles avec les résumés.';
+$string['screenshotof'] = 'Capture d\'écran de';
+$string['setchannel'] = 'Flux de formation';
+$string['setchannel_desc'] = 'Cochez cette case pour classer le cours  en tant que flux de formation.';
+$string['setchannel_help'] = 'Les flux de formation sont des cours spéciaux dans Agora qui permettent aux Experts en la Matière (Subject Matter Experts - SME) de présenter aux apprenants du contenu selectionné.';
+$string['settings'] = 'Paramètres';
+$string['tags'] = 'Tags';
+$string['tagshelp'] = 'utilisant les tags';
+$string['tagshelp_help'] = 'Vous pouvez ajouter plusieurs tags en les séparant avec une virgule, par exemple « tag un, montag, tag trois etc. »';
+$string['templatedeleted'] = 'Le modèle a été supprimé avec succès';
+$string['title'] = 'Modèles {$a}';
+$string['updatetemplate'] = 'Mettre à jour le modèle';
+$string['visibility'] = 'Visibilité';
+$string['visibility_help'] = 'Voulez-vous rendre ce cours visible ?';
+$string['visiblename'] = 'Formats de cours';
