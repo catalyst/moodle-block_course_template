1ad04d8775f690876013ff65bd95788b24e1bf4c
Stacey Walker <stacey@catalyst-eu.net>
2016-04-06 10:41:34 +0100

    WR252254: added Arabic language translation
    Change-Id: I9e3aff2d1498b7a78d6bf6b9c04e3173cec9fef0

---
+++ lang/ar/block_course_template.php
@@ -0,0 +1,118 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://academydev.catalystdemo.co.uk
+ *
+ * @package    block
+ * @subpackage course_template
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['actions'] = 'الإجراءات';
+$string['activetags'] = 'البطاقات النشطة';
+$string['addtemplate'] = 'إضافة قالب للمقرر';
+$string['alltemplates'] = 'عرض كل قوالب المقرر';
+$string['basedon'] = 'استنادا على \'{$a}\'';
+$string['basedoncourse'] = 'استنادا على مقرر \'{$a}\'';
+$string['basedontemplate'] = 'استنادا على قالب \'{$a}\'';
+$string['channeltemplates'] = 'قوالب القناة';
+$string['configdescription'] = 'تحديد أي أشكال المقرر يمكن استخدامها لإنشاء قوالب المقرر';
+$string['confirmdelete'] = 'هل أنت متأكد انك تريد حذف القالب \'{$a}\'?';
+$string['courseformats'] = 'أشكال المقرر';
+$string['course_template:addinstance'] = 'إضافة كتلة قالب مقرر';
+$string['course_template:createcourse'] = 'إنشاء مقررجديد من قالب بمقرر';
+$string['course_template:duplicatecourse'] = 'تكرار المقرر';
+$string['course_template:edit'] = 'إنشاء وإدارة قوالب المقرر';
+$string['course_template:import'] = 'استيراد قالب لمقرر';
+$string['course_template:managechannels'] = 'إدارة قنوات التعليم';
+$string['course_template:myaddinstance'] = 'إضافة كتلة قالب مقرر';
+$string['coursetemplates'] = 'قةالب المقرر';
+$string['course_template:view'] = 'عرض قوالب المقرر المتوفرة';
+$string['course_template:viewchannel'] = 'عرض قناة التعليم';
+$string['createcourse'] = 'مقرر جديد من قالب';
+$string['createdsuccessfully'] = 'تم إنشاء المقرر بنجاح';
+$string['createtemplate'] = 'إنشاء قالب';
+$string['customcourseheading'] = 'عنوان قناة التعليم';
+$string['customcourseheading_help'] = 'يمكن لقنوات التعلم أن يكون لها عناوين نمط مخصصة. وإذا لم يتم إضافة شيئ هنا، سيتم استخدام الاسم الكامل للمقرر.';
+$string['deletetemplate'] = 'حذف القالب';
+$string['details'] = 'تفاصيل';
+$string['duplicatecourse'] = 'تكرار المقرر';
+$string['duplicatecoursesuccess'] = 'تم تكرار المقرر. <BR> الرجاء، تعديل المقرر أدناه.';
+$string['edittempdata'] = 'تعديل بيانات المقرر';
+$string['edittemplate'] = 'تعديل قالب المقرر';
+$string['edittmptitle'] = 'تعديل قالب المقرر';
+$string['enrolment'] = 'تسجيل';
+$string['enrolment_help'] = 'هل ترغب في نسخ بيانات التسجيل للمستخدم؟';
+$string['error:couldnotinserttag'] = 'تعذر إدراج علامة {$a}';
+$string['error:couldntgetcourse'] = 'تعذر العثور على مقرر برقم \'{$a}\'';
+$string['error:couldntgettag'] = 'تعذر العثور على علامة برقم \'{$a}\'';
+$string['error:couldntupdate'] = 'تعذر تحديث قالب المقرر \'{$a}\'';
+$string['error:createtemplatefile'] = 'تعذر إنشاء ملف النسخ الاحتياطي لقالب \'{$a}\'';
+$string['error:deleteinst'] = 'تعذر حذف حالات العلامة';
+$string['error:deletetag'] = 'تعذر حذف العلامات';
+$string['error:extractarchive'] = 'تعذراستخراج الأرشيف';
+$string['error:movearchive'] = 'تعذر نسخ استعادة الأرشيف';
+$string['error:nameempty'] = 'حقل الاسم لا يكون فارغ.';
+$string['error:nametaken'] = 'اسم قالب قيد الاستخدام. الرجاء اختيار آخر.';
+$string['error:needsconfig'] = 'لم يتم تكوين النسخ الاحتياطي التلقائي، وهو مطلوب لقوالب المقرر للعمل. الرجاء سؤال أحد المسؤولين لتحديد هذا الأمر.';
+$string['error:nodirectory'] = 'لا يوجد دليل قالب احتياطي';
+$string['error:notemplate'] = 'لا يوجد سجل رقم قالب {$a}';
+$string['error:notemplates'] = 'لا توجد قوالب مقرر متاحة لإنشاء مقرر';
+$string['error:paramrequired'] = 'معلمة مفقودة - يجب تحديد مقرر أو قالب';
+$string['error:processerror'] = 'تعذر معالجة ملف الأرشيف.';
+$string['error:save'] = 'تعذر حفظ القالب';
+$string['error:sitecourse'] = 'لا يجوز استخدام سياق موقع المقرر.';
+$string['existingtags'] = 'العلامات المتوفرة';
+$string['filtertemplates'] = 'تحديث القوالب';
+$string['filtertext'] = 'عرض القوالب ذات الكلمات الدلالية مع ما يلي:';
+$string['goto'] = 'اذهب الى';
+$string['importedsuccessfully'] = 'تم استيراد مقررك بنجاح';
+$string['importintocourse'] = 'استيراد القالب للمقرر';
+$string['intocourse'] = 'استيراد القالب لهذا المقرر';
+$string['lastmodifiedon'] = 'آخر تعديل: {$a}';
+$string['name'] = 'اسم المقرر';
+$string['name_help'] = 'يرجى توفير اسم للدورة الجديدة';
+$string['newcoursefromtemp'] = 'مقرر جديد من القالب';
+$string['newcoursetemplate'] = 'قالب دورة جديدة';
+$string['newtemplate'] = 'قالب من هذه الدورة';
+$string['noimage'] = 'لا توجد صورة';
+$string['norecords'] = 'لا توجد قوالب لعرضها';
+$string['notags'] = 'لا توجد حاليا أي علامات محددة.';
+$string['pagesize'] = 'حجم الصفحة';
+$string['pagesize_desc'] = 'ضبط عدد القوالب التي ينبغي أن تظهر في كل صفحة.';
+$string['pluginname'] = 'قوالب المقرر';
+$string['preview'] = 'معاينة';
+$string['savesuccess'] = 'تم حفظ إعدادات القالب بنجاح';
+$string['screenshot'] = 'لقطة';
+$string['screenshot_help'] = 'يتم عرض اللقطات، مثل الصور، في قائمة القوالب جنبا إلى جنب مع ملخص.';
+$string['screenshotof'] = 'لقطة من';
+$string['setchannel'] = 'قناة التعليم';
+$string['setchannel_desc'] = 'إذا حددت، سيكون تصنيف هذه الدورة بمثابة قناة التعلم.';
+$string['setchannel_help'] = 'قنوات التعلم هي دورات خاصة في أغورا التي تسمح للخبراء في الموضوع لتقديم المحتوى المحدد إلى المتعلمين.';
+$string['settings'] = 'إعدادات';
+$string['tags'] = 'علامات';
+$string['tagshelp'] = 'استخدام العلامات';
+$string['tagshelp_help'] = 'يمكنك إضافة العديد من العلامات مفصولة بفاصلة على سبيل المثال العلامة الأولى، العلامة الثانية، العلامة الثالثة الخ';
+$string['templatedeleted'] = 'تم حذف القالب بنجاح';
+$string['title'] = '{$a} قوالب';
+$string['updatetemplate'] = 'تحديث القالب';
+$string['visibility'] = 'الرؤية';
+$string['visibility_help'] = 'هل ترغب في جعل المقررمرئي؟';
+$string['visiblename'] = 'أشكال المقرر';
